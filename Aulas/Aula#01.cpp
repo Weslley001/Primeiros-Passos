cfc94c2854e2eb7227c9fcf5fb77bf976f068724
Weslley Romão <weslley.romao001@gmail.com>
2023-04-16 14:45:12 -0700

    course commit

--- Aulas/Aula#01.cpp
+++ Aulas/Aula#01.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main(){
 
-    cout << "Hello, World!" << endl << "Ola mundo (para os leigos)" << endl;
+    cout << "Hello, World!" << endl;
 
-    system("pause");
+    cout << "Ola, Mundo!" << endl;
+
+    cout << "Pressione qualquer tecla para sair...";
+
+    system ("pause > null");
 
     return 0;
 }
---
+++ Aulas/Aula#02.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // INT: Essa variavel armazena numeros inteiros apenas.
+
+    int inteiro;
+    inteiro = 5;
+    cout << inteiro << endl;
+
+    // FLOAT: Essa variavel armazena numeros reais que vão de 1.2e-38 a 3.4e-38.
+
+    float real;
+    real = 5.2;
+    cout << real << endl;
+
+    // DOUBLE: Essa variavel armazena numeros reais que vão de 2.2e-308 a 1.79e308.
+
+    double real2;
+    real2 = 5.2e99;
+
+    cout << real2 << endl;
+    // BOOL: Essa variavel armazena valores True = 1 ou False = 0.
+
+    bool booleano;
+    booleano = false;
+    cout << booleano << endl;
+
+    // CHAR: Essa variavel armazena uma caractere apenas.
+
+    char letra;
+    letra = 'b';
+    cout << letra << endl;
+
+    // STRING: Essa variavel armazena uma cadeia de carateres.
+
+    string palavra;
+    palavra = "bola";
+    cout << palavra << endl;
+
+    int idade;
+    cout << "Qual e sua idade?" << endl;
+    cin >> idade;
+    cout << "idade: " << idade << endl;
+
+    return 0;
+}
