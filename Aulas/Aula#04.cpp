fa2519ffb76cb81d86fed062a60e7a23db2b1e4a
Weslley Romão <weslley.romao001@gmail.com>
2023-04-19 10:31:40 -0700

    Course commit

---
+++ Aulas/Aula#04.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.14;
+#define aprendendo cout << "Estou apredendo programacao" << endl;
+
+int t;
+
+void welcome(){
+    cout << "Sejam bem vindos!!!" << endl;
+
+}
+
+int somar(int x, int y){
+
+    //int t;
+    t = 1;
+
+    int soma; // Essa variavel sera uma variavel local, ela so valera dentro dessa função
+    soma = x+y;
+
+    return soma;
+}
+
+int main(){
+
+    //int t;
+    //t = 0;
+
+    welcome();
+
+    int a=5, b=4;
+    int s;
+    s = somar(a,b);
+    cout << "soma: " << s << endl;
+
+    cout << t;
+
+    //cout << pi;
+    aprendendo;
+
+    return 0;
+}
